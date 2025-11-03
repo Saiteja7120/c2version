e90901d4c16770884d2e95db44345e9e60fe7f34
Saiteja7120 <saitejavempatapu11@gmail.com>
2025-11-03 15:26:21 +0530

    Update c1.tf

--- c1.tf
+++ c1.tf
@@ -1,20 +1,30 @@
 # Terraform Block
-terraform {
-  required_version = ">= 1.6" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.0"
-    }
-  }
-}
 
-# Provider Block
-provider "aws" {
-  region  = var.aws_region
-  profile = "default"
+terraform {
+
+  required_version = ">= 0.14, <= 2.0.0" # Allows OpenTofu versions from 0.14 up to 2.0
+
+  required_providers {
+
+    aws = {
+
+      source  = "hashicorp/aws"
+
+      version = "~> 3.0"  # Allows AWS provider versions >= 3.0 and < 4.0
+
+    }
+
+  }
+
 }
-/*
-Note-1:  AWS Credentials Profile (profile = "default") configured on your local desktop terminal  
-$HOME/.aws/credentials
-*/
+ 
+# Provider Block
+
+provider "aws" {
+
+  region  = var.aws_region
+
+  profile = "default"
+
+}
+ 
